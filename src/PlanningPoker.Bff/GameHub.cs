33926507b4a981de83f189191ac32eb0a7754cae
Angelito Casagrande <angelito@angelito.com.br>
2022-09-03 23:28:27 -0300

    Update hub

--- src/PlanningPoker.Bff/GameHub.cs
+++ src/PlanningPoker.Bff/GameHub.cs
@@ -15,7 +15,9 @@ namespace PlanningPoker.Bff
             _memoryCache = memoryCache;
         }
 
-        private void Log(object data) => Console.WriteLine($"[{DateTime.Now:yyyy-MM-dd HH:mm:ss:fff}] {JsonConvert.SerializeObject(data)}");
+        #region Private methods
+
+        private static void Log(object data) => Console.WriteLine($"[{DateTime.Now:yyyy-MM-dd HH:mm:ss:fff}] {JsonConvert.SerializeObject(data)}");
 
         private GameSession UpdateCache(GameSession gameSession)
         {
@@ -38,14 +40,18 @@ namespace PlanningPoker.Bff
                 return gameSessionError;
             }
 
+            if (string.IsNullOrWhiteSpace(gameSession.Owner.Name))
+            {
+                _memoryCache.Remove(sessionId);
+                var gameSessionError = GameSession.CreateGameSessionError($"Invalid session {sessionId} from cache.");
+                return gameSessionError;
+            }
+
             return gameSession;
         }
 
         private async Task NotifyChangesToPlayers(GameSession gameSession)
         {
-            if (gameSession.HasError)
-                return;
-
             gameSession = UpdateCache(gameSession);
             await Groups.AddToGroupAsync(Context.ConnectionId, gameSession.Id);
             await Clients.Group(gameSession.Id).SendAsync(GameHubListeners.SessionContent, gameSession.ToString());
@@ -53,9 +59,6 @@ namespace PlanningPoker.Bff
 
         private async Task NotifySessionCreatedToCaller(GameSession gameSession)
         {
-            if (gameSession.HasError)
-                return;
-
             gameSession = UpdateCache(gameSession);
             await Groups.AddToGroupAsync(Context.ConnectionId, gameSession.Id);
             await Clients.Caller.SendAsync(GameHubListeners.SessionContent, gameSession.ToString());
@@ -71,6 +74,8 @@ namespace PlanningPoker.Bff
             await Clients.Caller.SendAsync(GameHubListeners.SessionError, gameSession.ToString());
         }
 
+        #endregion
+
         public async Task CreateSession(string ownerName)
         {
             Log($"{nameof(CreateSession)}-{nameof(ownerName)}: {ownerName}");
@@ -78,6 +83,13 @@ namespace PlanningPoker.Bff
             var gameSession = new GameSession(ownerName);
             await NotifySessionCreatedToCaller(gameSession);
             await NotifyErrorToCaller(gameSession);
+        }
+
+        public bool HasSession(string sessionId)
+        {
+            Log($"{nameof(HasSession)}-{nameof(sessionId)}");
+            var gameSession = GetGameSessionFromCache(sessionId);
+            return !gameSession.HasError;
         }
 
         public async Task JoinSession(string sessionId, string playerName)
@@ -97,6 +109,17 @@ namespace PlanningPoker.Bff
 
             var gameSession = GetGameSessionFromCache(sessionId);
             gameSession.SetPlayerMove(playerName, number);
+
+            await NotifyChangesToPlayers(gameSession);
+            await NotifyErrorToCaller(gameSession);
+        }
+
+        public async Task UpdateDescription(string sessionId, string playerName, string description)
+        {
+            Log($"{nameof(StartSession)}-{nameof(sessionId)}: {sessionId} | {nameof(playerName)}: {playerName} | {nameof(description)}: {description}");
+
+            var gameSession = GetGameSessionFromCache(sessionId);
+            gameSession.UpdateDescription(playerName, description);
 
             await NotifyChangesToPlayers(gameSession);
             await NotifyErrorToCaller(gameSession);
